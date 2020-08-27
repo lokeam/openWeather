0a7b1e9980a08b92ce4a15e3d9767ba815ef6f8b
Ahnming Loke <lokeahnming@gmail.com>
2020-08-27 16:44:53 -0400

    feat: added moment dependency to cover for missing api time datapoints

--- src/components/CurrentView/CurrentView.jsx
+++ src/components/CurrentView/CurrentView.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import useFetch from '../../helpers/useFetch';
+import moment from 'moment'
 import './current.css';
 
 function CurrentView( {geoData} ) {
 
   const url = `http://api.openweathermap.org/data/2.5/weather?lat=${geoData.lat}&lon=${geoData.lon}&units=imperial&appid=${process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY}`;
   const { status, data, error } = useFetch(url);
-  console.log('CurrentView, fetch status: ', status);
+  let currentDateTime = moment().format('MMMM Do YYYY');
+
   console.log('CurrentView, fetch data: ', data);
+  console.log('CurrentView, fetch status: ', status);
+  console.log('CurrentView, currentDateTime: ', currentDateTime);
 
   return (
     <div className="weather-container--current">
